9ef2ab41bd0b29fb2b63ac3e1d9a8e386d64611d
amikizzy <105018813+amikizzy@users.noreply.github.com>
2026-02-08 13:33:34 +0000

    Add Kitty configuration with shell integration
    Added Kitty configuration with shell integration and custom settings.

--- config/kitty/kitty.nix
+++ config/kitty/kitty.nix
@@ -1,2 +1,20 @@
-# Kitty config
-font_family JetBrains Mono
+{
+  programs.kitty = {
+    enable = true;
+    shellIntegration.enableFishIntegration = true;
+    settings = {
+      include = "./theme.conf";
+      background_opacity = 0.8;
+      confirm_os_window_close = 0;
+      cursor_trail = 3;
+      font_family = "JetBrains Mono Nerd Font";
+      bold_font = "auto";
+      italic_font = "auto";
+      bold_italic_font = "auto";
+    };
+    # keybindings = {
+      # "ctrl+c" = "copy_to_clipboard";
+      # "ctrl+v" = "paste_from_clipboard";
+    # };
+  };
+}
